dea9071a244c2af1b723d883c3d38fc69efd7888
Yudongseok <rex010307@gmail.com>
2025-03-05 15:23:14 +0900

    수정정

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -4,11 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.shop.cafe.dto.Member;
 import com.shop.cafe.service.MemberService;
